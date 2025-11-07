987bd6aa9709ef99911126b0f3487144afbb76c5
Parcifal <89307076692roman@gmail.com>
2025-11-07 13:43:10 +0300

    docs: add Transactions (1)

--- src/main/java/com/example/Pastach/repository/PostRepository.java
+++ src/main/java/com/example/Pastach/repository/PostRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    List<Post> findPostsByAuthorId(String authorId);
     Page<Post> findByAuthorId(String authorId, Pageable pageable);
 
     Page<Post> findAll(Pageable pageable);
