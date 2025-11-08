9b1bcb3d78b567933ec1dc8d75c49302b71c329a
Parcifal <89307076692roman@gmail.com>
2025-11-09 00:14:27 +0300

    typo: PostNotFoundException

--- src/main/java/com/example/Pastach/exception/PostNotFoundException.java
+++ src/main/java/com/example/Pastach/exception/PostNotFoundException.java
@@ -2,6 +2,6 @@ package com.example.Pastach.exception;
 
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(int id) {
-        super("Post with id " + id + " not found" + id);
+        super("Post with id " + id + " not found");
     }
 }
